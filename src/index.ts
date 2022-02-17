e9c140c732343fdcd9f4839ddd140133af6e6d8c
electrovir <electrovir@users.noreply.github.com>
2022-02-17 06:41:27 -0700

    update deps and configs

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import {fileExtensionRule} from './rules/file-extension/file-extension.rule';
 import {fileNameStartsWithRule} from './rules/file-name-starts-with/file-name-starts-with.rule';
 import {importAsReferenceRule} from './rules/import-as-reference/import-as-reference.rule';
 
-export default [fileNameStartsWithRule, importAsReferenceRule, fileExtensionRule];
+export default [
+    fileNameStartsWithRule,
+    importAsReferenceRule,
+    fileExtensionRule,
+];
